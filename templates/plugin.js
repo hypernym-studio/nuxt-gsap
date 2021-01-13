ee9c68ea5ccd690a07b8352ed0d4069900db38be
Ivo Dolenc <ivodolenc@gmail.com>
2021-01-13 14:57:00 +0100

    refactor: rename '_options' to 'options'

--- templates/plugin.js
+++ templates/plugin.js
@@ -2,23 +2,23 @@ import Vue from 'vue'
 import { gsap } from 'gsap'
 
 Vue.directive('gsap', (el, binding) => {
-  const _options = { ...binding.value }
+  const options = { ...binding.value }
   const modifiers = binding.modifiers
 
+  if (modifiers.set) {
+    gsap.set(el, options)
+  }
+
   if (modifiers.to) {
-    gsap.to(el, _options)
+    gsap.to(el, options)
   }
 
   if (modifiers.from) {
-    gsap.from(el, _options)
+    gsap.from(el, options)
   }
 
   if (modifiers.fromTo) {
     gsap.fromTo(el, { ...binding.value[0] }, { ...binding.value[1] })
-  }
-
-  if (modifiers.set) {
-    gsap.set(el, _options)
   }
 })
 
