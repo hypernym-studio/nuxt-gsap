9d08d5fca9f71b9fa34e038acec0c8e270a998e1
Ivo Dolenc <ivodolenc@gmail.com>
2021-01-25 14:21:32 +0100

    docs: add 'staggering' example

---
+++ examples/staggering/nuxt.config.js
@@ -0,0 +1,3 @@
+export default {
+  buildModules: ['nuxt-gsap-module']
+}
---
+++ examples/staggering/pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="box"></div>
+    <div class="box"></div>
+    <div class="box"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.staggering()
+  },
+  methods: {
+    staggering() {
+      const gsap = this.$gsap
+
+      gsap.to('.box', {
+        x: 300,
+        stagger: 0.1
+      })
+    }
+  }
+}
+</script>
