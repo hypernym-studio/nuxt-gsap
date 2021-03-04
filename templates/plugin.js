5032dbfa04c30c957cdd37b90a975ea2fa7e32d3
Ivo Dolenc <ivodolenc@gmail.com>
2021-03-04 14:45:42 +0100

    fix: avoid possible errors when activating plugins

--- templates/plugin.js
+++ templates/plugin.js
@@ -32,70 +32,67 @@ Vue.prototype.$gsap = gsap
       <% if (options.extraPlugins.cssRule) { %>
         const { CSSRulePlugin } = require('gsap/CSSRulePlugin')
         Vue.prototype.$CSSRulePlugin = CSSRulePlugin
+        gsap.registerPlugin(CSSRulePlugin)
       <% } %>
 
       <% if (options.extraPlugins.draggable) { %>
         const { Draggable } = require('gsap/Draggable')
         Vue.prototype.$Draggable = Draggable
+        gsap.registerPlugin(Draggable)
       <% } %>
 
       <% if (options.extraPlugins.easel) { %>
         const { EaselPlugin } = require('gsap/EaselPlugin')
         Vue.prototype.$EaselPlugin = EaselPlugin
+        gsap.registerPlugin(EaselPlugin)
       <% } %>
 
       <% if (options.extraPlugins.motionPath) { %>
         const { MotionPathPlugin } = require('gsap/MotionPathPlugin')
         Vue.prototype.$MotionPathPlugin = MotionPathPlugin
+        gsap.registerPlugin(MotionPathPlugin)
       <% } %>
 
       <% if (options.extraPlugins.pixi) { %>
         const { PixiPlugin } = require('gsap/PixiPlugin')
         Vue.prototype.$PixiPlugin = PixiPlugin
+        gsap.registerPlugin(PixiPlugin)
       <% } %>
 
       <% if (options.extraPlugins.text) { %>
         const { TextPlugin } = require('gsap/TextPlugin')
         Vue.prototype.$TextPlugin = TextPlugin
+        gsap.registerPlugin(TextPlugin)
       <% } %>
 
       <% if (options.extraPlugins.scrollTo) { %>
         const { ScrollToPlugin } = require('gsap/ScrollToPlugin')
         Vue.prototype.$ScrollToPlugin = ScrollToPlugin
+        gsap.registerPlugin(ScrollToPlugin)
       <% } %>
 
       <% if (options.extraPlugins.scrollTrigger) { %>
         const { ScrollTrigger } = require('gsap/ScrollTrigger')
         Vue.prototype.$ScrollTrigger = ScrollTrigger
+        gsap.registerPlugin(ScrollTrigger)
       <% } %>
 
       <% if (options.extraEases.expoScaleEase) { %>
         const { ExpoScaleEase } = require('gsap/EasePack')
         Vue.prototype.$ExpoScaleEase = ExpoScaleEase
+        gsap.registerPlugin(ExpoScaleEase)
       <% } %>
       
       <% if (options.extraEases.roughEase) { %>
         const { RoughEase } = require('gsap/EasePack')
         Vue.prototype.$RoughEase = RoughEase
+        gsap.registerPlugin(RoughEase)
       <% } %>
 
       <% if (options.extraEases.slowMo) { %>
         const { SlowMo } = require('gsap/EasePack')
         Vue.prototype.$SlowMo = SlowMo
+        gsap.registerPlugin(SlowMo)
       <% } %>
-
-        gsap.registerPlugin(
-          <%= options.extraPlugins.cssRule && 'CSSRulePlugin,' %> 
-          <%= options.extraPlugins.draggable && 'Draggable,' %> 
-          <%= options.extraPlugins.easel && 'EaselPlugin,' %> 
-          <%= options.extraPlugins.motionPath && 'MotionPathPlugin,' %> 
-          <%= options.extraPlugins.pixi && 'PixiPlugin,' %> 
-          <%= options.extraPlugins.text && 'TextPlugin,' %> 
-          <%= options.extraPlugins.scrollTo && 'ScrollToPlugin,' %> 
-          <%= options.extraPlugins.scrollTrigger && 'ScrollTrigger,' %> 
-          <%= options.extraEases.expoScaleEase && 'ExpoScaleEase,' %> 
-          <%= options.extraEases.roughEase && 'RoughEase,' %> 
-          <%= options.extraEases.slowMo && 'SlowMo,' %>
-        )
     }
 <% } %>
