fead851abfd1e5cd262d92c14aad6aff971a20a4
Ivo Dolenc <ivodolenc@gmail.com>
2021-08-06 14:10:15 +0200

    feat: add new 'registerEffect' option to simplify global registration

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ export default function gsapModule(moduleOptions) {
     extraPlugins: {},
     extraEases: {},
     clubPlugins: {},
+    registerEffect: [],
 
     ...nuxt.options.gsap,
     ...moduleOptions
--- templates/plugin.js
+++ templates/plugin.js
@@ -22,158 +22,168 @@ Vue.directive('gsap', (el, binding) => {
   }
 })
 
-Vue.prototype.$gsap = gsap
+<% if (options.registerEffect.length) { %>
+  const effects = <%= serialize(options.registerEffect) %>
 
-<% if (
-  Object.keys(options.extraPlugins).length ||
-  Object.keys(options.extraEases).length ||
-  Object.keys(options.clubPlugins).length
-  ) { %>
-    if (process.client) {
-      <% if (options.extraPlugins.cssRule) { %>
-        const { CSSRulePlugin } = require('gsap/CSSRulePlugin')
-        Vue.prototype.$CSSRulePlugin = CSSRulePlugin
-        gsap.registerPlugin(CSSRulePlugin)
-      <% } %>
-
-      <% if (options.extraPlugins.draggable) { %>
-        const { Draggable } = require('gsap/Draggable')
-        Vue.prototype.$Draggable = Draggable
-        gsap.registerPlugin(Draggable)
-      <% } %>
-
-      <% if (options.extraPlugins.easel) { %>
-        const { EaselPlugin } = require('gsap/EaselPlugin')
-        Vue.prototype.$EaselPlugin = EaselPlugin
-        gsap.registerPlugin(EaselPlugin)
-      <% } %>
-
-      <% if (options.extraPlugins.motionPath) { %>
-        const { MotionPathPlugin } = require('gsap/MotionPathPlugin')
-        Vue.prototype.$MotionPathPlugin = MotionPathPlugin
-        gsap.registerPlugin(MotionPathPlugin)
-      <% } %>
-
-      <% if (options.extraPlugins.pixi) { %>
-        const { PixiPlugin } = require('gsap/PixiPlugin')
-        Vue.prototype.$PixiPlugin = PixiPlugin
-        gsap.registerPlugin(PixiPlugin)
-      <% } %>
-
-      <% if (options.extraPlugins.text) { %>
-        const { TextPlugin } = require('gsap/TextPlugin')
-        Vue.prototype.$TextPlugin = TextPlugin
-        gsap.registerPlugin(TextPlugin)
-      <% } %>
-
-      <% if (options.extraPlugins.scrollTo) { %>
-        const { ScrollToPlugin } = require('gsap/ScrollToPlugin')
-        Vue.prototype.$ScrollToPlugin = ScrollToPlugin
-        gsap.registerPlugin(ScrollToPlugin)
-      <% } %>
-
-      <% if (options.extraPlugins.scrollTrigger) { %>
-        const { ScrollTrigger } = require('gsap/ScrollTrigger')
-        Vue.prototype.$ScrollTrigger = ScrollTrigger
-        gsap.registerPlugin(ScrollTrigger)
-      <% } %>
-
-      <% if (options.extraEases.expoScaleEase) { %>
-        const { ExpoScaleEase } = require('gsap/EasePack')
-        Vue.prototype.$ExpoScaleEase = ExpoScaleEase
-        gsap.registerPlugin(ExpoScaleEase)
-      <% } %>
-      
-      <% if (options.extraEases.roughEase) { %>
-        const { RoughEase } = require('gsap/EasePack')
-        Vue.prototype.$RoughEase = RoughEase
-        gsap.registerPlugin(RoughEase)
-      <% } %>
-
-      <% if (options.extraEases.slowMo) { %>
-        const { SlowMo } = require('gsap/EasePack')
-        Vue.prototype.$SlowMo = SlowMo
-        gsap.registerPlugin(SlowMo)
-      <% } %>
-
-      // Club Plugins
-
-      <% if (options.clubPlugins.customEase) { %>
-        const { CustomEase } = require('gsap/CustomEase')
-        Vue.prototype.$CustomEase = CustomEase
-        gsap.registerPlugin(CustomEase)
-      <% } %>
-
-      <% if (options.clubPlugins.customBounce) { %>
-        const { CustomBounce } = require('gsap/CustomBounce')
-        Vue.prototype.$CustomBounce = CustomBounce
-        gsap.registerPlugin(CustomBounce)
-      <% } %>
-
-      <% if (options.clubPlugins.customWiggle) { %>
-        const { CustomWiggle } = require('gsap/CustomWiggle')
-        Vue.prototype.$CustomWiggle = CustomWiggle
-        gsap.registerPlugin(CustomWiggle)
-      <% } %>
-
-      <% if (options.clubPlugins.drawSVG) { %>
-        const { DrawSVGPlugin } = require('gsap/DrawSVGPlugin')
-        Vue.prototype.$DrawSVGPlugin = DrawSVGPlugin
-        gsap.registerPlugin(DrawSVGPlugin)
-      <% } %>
-
-      <% if (options.clubPlugins.flip) { %>
-        const { Flip } = require('gsap/Flip')
-        Vue.prototype.$Flip = Flip
-        gsap.registerPlugin(Flip)
-      <% } %>
-
-      <% if (options.clubPlugins.gsDevTools) { %>
-        const { GSDevTools } = require('gsap/GSDevTools')
-        Vue.prototype.$GSDevTools = GSDevTools
-        gsap.registerPlugin(GSDevTools)
-      <% } %>
-
-      <% if (options.clubPlugins.inertia) { %>
-        const { InertiaPlugin } = require('gsap/InertiaPlugin')
-        Vue.prototype.$InertiaPlugin = InertiaPlugin
-        gsap.registerPlugin(InertiaPlugin)
-      <% } %>
-
-      <% if (options.clubPlugins.morphSVG) { %>
-        const { MorphSVGPlugin } = require('gsap/MorphSVGPlugin')
-        Vue.prototype.$MorphSVGPlugin = MorphSVGPlugin
-        gsap.registerPlugin(MorphSVGPlugin)
-      <% } %>
-
-      <% if (options.clubPlugins.motionPathHelper) { %>
-        const { MotionPathHelper } = require('gsap/MotionPathHelper')
-        Vue.prototype.$MotionPathHelper = MotionPathHelper
-        gsap.registerPlugin(MotionPathHelper)
-      <% } %>
-
-      <% if (options.clubPlugins.physics2D) { %>
-        const { Physics2DPlugin } = require('gsap/Physics2DPlugin')
-        Vue.prototype.$Physics2DPlugin = Physics2DPlugin
-        gsap.registerPlugin(Physics2DPlugin)
-      <% } %>
-
-      <% if (options.clubPlugins.physicsProps) { %>
-        const { PhysicsPropsPlugin } = require('gsap/PhysicsPropsPlugin')
-        Vue.prototype.$PhysicsPropsPlugin = PhysicsPropsPlugin
-        gsap.registerPlugin(PhysicsPropsPlugin)
-      <% } %>
-
-      <% if (options.clubPlugins.scrambleText) { %>
-        const { ScrambleTextPlugin } = require('gsap/ScrambleTextPlugin')
-        Vue.prototype.$ScrambleTextPlugin = ScrambleTextPlugin
-        gsap.registerPlugin(ScrambleTextPlugin)
-      <% } %>
-
-      <% if (options.clubPlugins.splitText) { %>
-        const { SplitText } = require('gsap/SplitText')
-        Vue.prototype.$SplitText = SplitText
-        gsap.registerPlugin(SplitText)
-      <% } %>
-    }
+  effects.forEach(effect => {
+    gsap.registerEffect(effect)
+  })
 <% } %>
+
+export default ({ app }, inject) => {
+  inject('gsap', gsap)
+
+  <% if (
+    Object.keys(options.extraPlugins).length ||
+    Object.keys(options.extraEases).length ||
+    Object.keys(options.clubPlugins).length
+    ) { %>
+      if (process.client) {
+        <% if (options.extraPlugins.cssRule) { %>
+          const { CSSRulePlugin } = require('gsap/CSSRulePlugin')
+          inject('CSSRulePlugin', CSSRulePlugin)
+          gsap.registerPlugin(CSSRulePlugin)
+          <% } %>
+          
+          
+        <% if (options.extraPlugins.draggable) { %>
+          const { Draggable } = require('gsap/Draggable')
+          inject('Draggable', Draggable)
+          gsap.registerPlugin(Draggable)
+        <% } %>
+
+        <% if (options.extraPlugins.easel) { %>
+          const { EaselPlugin } = require('gsap/EaselPlugin')
+          inject('EaselPlugin', EaselPlugin)
+          gsap.registerPlugin(EaselPlugin)
+        <% } %>
+
+        <% if (options.extraPlugins.motionPath) { %>
+          const { MotionPathPlugin } = require('gsap/MotionPathPlugin')
+          inject('MotionPathPlugin', MotionPathPlugin)
+          gsap.registerPlugin(MotionPathPlugin)
+        <% } %>
+
+        <% if (options.extraPlugins.pixi) { %>
+          const { PixiPlugin } = require('gsap/PixiPlugin')
+          inject('PixiPlugin', PixiPlugin)
+          gsap.registerPlugin(PixiPlugin)
+        <% } %>
+
+        <% if (options.extraPlugins.text) { %>
+          const { TextPlugin } = require('gsap/TextPlugin')
+          inject('TextPlugin', TextPlugin)
+          gsap.registerPlugin(TextPlugin)
+        <% } %>
+
+        <% if (options.extraPlugins.scrollTo) { %>
+          const { ScrollToPlugin } = require('gsap/ScrollToPlugin')
+          inject('ScrollToPlugin', ScrollToPlugin)
+          gsap.registerPlugin(ScrollToPlugin)
+        <% } %>
+
+        <% if (options.extraPlugins.scrollTrigger) { %>
+          const { ScrollTrigger } = require('gsap/ScrollTrigger')
+          inject('ScrollTrigger', ScrollTrigger)
+          gsap.registerPlugin(ScrollTrigger)
+        <% } %>
+
+        <% if (options.extraEases.expoScaleEase) { %>
+          const { ExpoScaleEase } = require('gsap/EasePack')
+          inject('ExpoScaleEase', ExpoScaleEase)
+          gsap.registerPlugin(ExpoScaleEase)
+        <% } %>
+        
+        <% if (options.extraEases.roughEase) { %>
+          const { RoughEase } = require('gsap/EasePack')
+          inject('RoughEase', RoughEase)
+          gsap.registerPlugin(RoughEase)
+        <% } %>
+
+        <% if (options.extraEases.slowMo) { %>
+          const { SlowMo } = require('gsap/EasePack')
+          inject('SlowMo', SlowMo)
+          gsap.registerPlugin(SlowMo)
+        <% } %>
+
+        // Club Plugins
+        <% if (options.clubPlugins.customEase) { %>
+          const { CustomEase } = require('gsap/CustomEase')
+          inject('CustomEase', CustomEase)
+          gsap.registerPlugin(CustomEase)
+        <% } %>
+
+        <% if (options.clubPlugins.customBounce) { %>
+          const { CustomBounce } = require('gsap/CustomBounce')
+          inject('CustomBounce', CustomBounce)
+          gsap.registerPlugin(CustomBounce)
+        <% } %>
+
+        <% if (options.clubPlugins.customWiggle) { %>
+          const { CustomWiggle } = require('gsap/CustomWiggle')
+          inject('CustomWiggle', CustomWiggle)
+          gsap.registerPlugin(CustomWiggle)
+        <% } %>
+
+        <% if (options.clubPlugins.drawSVG) { %>
+          const { DrawSVGPlugin } = require('gsap/DrawSVGPlugin')
+          inject('DrawSVGPlugin', DrawSVGPlugin)
+          gsap.registerPlugin(DrawSVGPlugin)
+        <% } %>
+
+        <% if (options.clubPlugins.flip) { %>
+          const { Flip } = require('gsap/Flip')
+          inject('Flip', Flip)
+          gsap.registerPlugin(Flip)
+        <% } %>
+
+        <% if (options.clubPlugins.gsDevTools) { %>
+          const { GSDevTools } = require('gsap/GSDevTools')
+          inject('GSDevTools', GSDevTools)
+          gsap.registerPlugin(GSDevTools)
+        <% } %>
+
+        <% if (options.clubPlugins.inertia) { %>
+          const { InertiaPlugin } = require('gsap/InertiaPlugin')
+          inject('InertiaPlugin', InertiaPlugin)
+          gsap.registerPlugin(InertiaPlugin)
+        <% } %>
+
+        <% if (options.clubPlugins.morphSVG) { %>
+          const { MorphSVGPlugin } = require('gsap/MorphSVGPlugin')
+          inject('MorphSVGPlugin', MorphSVGPlugin)
+          gsap.registerPlugin(MorphSVGPlugin)
+        <% } %>
+
+        <% if (options.clubPlugins.motionPathHelper) { %>
+          const { MotionPathHelper } = require('gsap/MotionPathHelper')
+          inject('MotionPathHelper', MotionPathHelper)
+          gsap.registerPlugin(MotionPathHelper)
+        <% } %>
+
+        <% if (options.clubPlugins.physics2D) { %>
+          const { Physics2DPlugin } = require('gsap/Physics2DPlugin')
+          inject('Physics2DPlugin', Physics2DPlugin)
+          gsap.registerPlugin(Physics2DPlugin)
+        <% } %>
+
+        <% if (options.clubPlugins.physicsProps) { %>
+          const { PhysicsPropsPlugin } = require('gsap/PhysicsPropsPlugin')
+          inject('PhysicsPropsPlugin', PhysicsPropsPlugin)
+          gsap.registerPlugin(PhysicsPropsPlugin)
+        <% } %>
+
+        <% if (options.clubPlugins.scrambleText) { %>
+          const { ScrambleTextPlugin } = require('gsap/ScrambleTextPlugin')
+          inject('ScrambleTextPlugin', ScrambleTextPlugin)
+          gsap.registerPlugin(ScrambleTextPlugin)
+        <% } %>
+
+        <% if (options.clubPlugins.splitText) { %>
+          const { SplitText } = require('gsap/SplitText')
+          inject('SplitText', SplitText)
+          gsap.registerPlugin(SplitText)
+        <% } %>
+      }
+  <% } %>
+}
